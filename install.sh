8bc5103e9d5601367953c2c5067c4a0d40ab8ab0
Clayton Smith <argilo@gmail.com>
2020-03-27 08:38:09 -0400

    Set locale and keyboard

--- install.sh
+++ install.sh
@@ -3,7 +3,9 @@
 set -e
 
 sudo raspi-config nonint do_blanking 1
+sudo raspi-config nonint do_change_locale en_US.UTF-8
 sudo raspi-config nonint do_change_timezone "America/Toronto"
+sudo raspi-config nonint do_configure_keyboard us
 sudo raspi-config nonint do_memory_split 128
 sudo raspi-config nonint do_overscan 1
 
