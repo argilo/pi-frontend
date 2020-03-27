e5ed49a45cd76738521eaaf9fc9807515d7badcf
Clayton Smith <argilo@gmail.com>
2020-03-26 21:50:09 -0400

    Initial commit

---
+++ install.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env sh
+
+set -e
+
+sudo raspi-config nonint do_blanking 1
+sudo raspi-config nonint do_memory_split 128
+sudo raspi-config nonint do_overscan 1
+
+if ! grep "myth30" /etc/apt/sources.list; then
+    sudo sh -c "echo deb http://dl.bintray.com/bennettpeter/deb/ buster myth30 >> /etc/apt/sources.list"
+    wget -O - https://bintray.com/user/downloadSubjectPublicKey?username=bintray | sudo apt-key add -
+fi
+sudo apt-get update
+sudo apt-get install -y \
+    ir-keytable \
+    lirc \
+    mythtv-light
+
+sudo cp /etc/lirc/lircd.conf.dist /etc/lirc/lircd.conf
+sudo cp /etc/lirc/lirc_options.conf.dist /etc/lirc/lirc_options.conf
+
+if [ ! -e ~/.config/autostart/mythtv.desktop ]; then
+    mkdir -p ~/.config/autostart
+    cp mythtv.desktop ~/.config/autostart/
+fi
+
+if [ ! -e ~/.mythtv/lircrc ]; then
+    mkdir -p ~/.mythtv
+    cp lircrc ~/.mythtv/
+fi
+
+if ! grep "ir-keytable" /etc/rc.local; then
+    sudo cp ir-keytable-hauppauge.cfg /etc/
+    sudo sed -i -e '$iir-keytable -p rc-5,rc-6\nir-keytable --write=/etc/ir-keytable-hauppauge.cfg\n' /etc/rc.local
+fi
