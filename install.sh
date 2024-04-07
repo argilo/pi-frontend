666375bca894a7f869b380a8d99d1d7017687200
Clayton Smith <argilo@gmail.com>
2024-04-06 23:27:11 -0400

    Remove unnecessary memory-saving measures

--- install.sh
+++ install.sh
@@ -71,12 +71,4 @@ if ! grep "scaling_governor" /etc/rc.local; then
     sudo systemctl disable raspi-config
 fi
 
-sudo systemctl disable cups
-sudo systemctl disable cups-browsed
-sudo systemctl disable ModemManager.service
-
-if ! grep "vm.swappiness=5" /etc/sysctl.conf; then
-    sudo sh -c "echo vm.swappiness=5 >> /etc/sysctl.conf"
-fi
-
 echo "Successfully configured MythTV."
